a291a9be2e3c3cf4a49f968c25343cd4cc9c9a09
Brian <gamerfunky78@gmail.com>
2024-10-11 14:32:27 +0300

    ongoing function with converter programme.

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,28 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useRef } from 'react'
+
 
 function App() {
+  const myRef = useRef();
   const [count, setCount] = useState(0)
+  const [degree, setDegree] = useState(0)
 
   return (
     <>
       <p className="text-xl font-bold text-center">
         React Experimentation.
       </p>
-      <br /><br /><br /><br />
-      <div>
+      <br /><br /><br />
+      <div className='bg-gray-300 p-5 rounded-2xl'>
+          <p className='font-bold text-center'>Counter Program.</p>
+          <br />
           <p>Count is {count}</p>
           <br /><br />
           <div className='flex flex-row justify-center items-center gap-5'>
-            <button className='bg-cyan-500 p-3' onClick={()=> setCount(count + 1)}>Increment</button>
-            <button className='bg-cyan-500 p-3' onClick={()=> setCount(count - 1)}>Decrement</button>
+            <button className='bg-cyan-500 p-3 rounded-xl' onClick={()=> setCount(count + 1)}>Increment</button>
+            <button className='bg-cyan-500 p-3 rounded-xl' onClick={()=> setCount(count - 1)}>Decrement</button>
           </div>
-            
+      </div>
+      <br /><br />
+      <div className='bg-gray-300 p-5 rounded-2xl'>
+        <p className='font-bold text-center'>Converter Programme ° to K (Degrees celcius To Kelvin)</p>
+        <br />
+        <p>{degree}</p>
+        <br />
 
+
+          <input type="text" placeholder='Input Value in Degrees or Kelvin' ref={myRef} className='bg-gray-200 p-3 rounded-lg'/>
+          <br /><br /><br />
+        <div className='flex flex-row justify-center items-center gap-5'>
           
-
+            <button className='bg-cyan-500 p-3 rounded-xl' onClick={()=> setDegree(degree + Number(myRef.current.value) + 274.15)}>Convert To Kelvin</button>
+            <button className='bg-cyan-500 p-3 rounded-xl'>Convert To Degree</button>
+          </div>
       </div>
       
     </>
